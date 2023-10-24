9ac48b15102381063c21cd961061b85b70e5ac4b
Mick Negenman <36791@ma-web.nl>
2023-10-24 18:44:29 +0200

    Mobile support

--- src/App.svelte
+++ src/App.svelte
@@ -10,11 +10,25 @@
 
 <style>
 	main {
+		box-sizing: border-box;
 		position: absolute;
 		transform: translate(-50%, 0);
 		left: 50%;
 		width: 30vw;
 		text-align: center;
 		color: white;
+	}
+
+	@media screen and (max-width: 1200px) {
+		main {
+			width: 50vw;
+		}
+	}
+
+	@media screen and (max-width: 680px) {
+		main {
+			width: 100vw;
+			padding: 0 10px 0px 10px;
+		}
 	}
 </style>
